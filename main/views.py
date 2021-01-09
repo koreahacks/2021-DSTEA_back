04b7a1c94e4d427bc4085c56c8c81b85aba5c166
hanchchch <cndghks15@gmail.com>
2021-01-09 13:36:02 +0900

    fix: changed write api method to POST

--- main/views.py
+++ main/views.py
@@ -67,14 +67,16 @@ def file_upload(request, board_url):
     return Message(Status.SUCCESS, page_num=page_num).res()
 
 def write(request, board_url):
+    if request.method != 'POST':
+        return Message(Status.FORBIDDEN, 'Method not allowed.')
     try:
         path = Path(board=Board.objects.get(board_url=board_url),
-                    page_id=request.GET['page_id'],
+                    page_id=request.POST['page_id'],
                     session_id=request.session['id'],
-                    is_public=request.GET['public'],
-                    pen_type=request.GET['pen_type'],
-                    color=request.GET['color'],
-                    data=request.GET['data']
+                    is_public=request.POST['public'],
+                    pen_type=request.POST['pen_type'],
+                    color=request.POST['color'],
+                    data=request.POST['data']
                     )
         path.save()
         return Message(Status.SUCCESS, is_valid=True).res()
