d6301657c22c7a2bb912b90b91f364f8e3ae520a
hanchchch <cndghks15@gmail.com>
2021-01-09 14:36:31 +0900

    fix: for develop environment

--- everyboard_back/wsgi.py
+++ everyboard_back/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'everyboard_back.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'everyboard_back.development')
 
 application = get_wsgi_application()
