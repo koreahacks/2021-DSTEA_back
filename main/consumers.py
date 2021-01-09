0de976f3506d33a7a7b5bce53d22f6b90deaf7af
hanchchch <cndghks15@gmail.com>
2021-01-09 22:21:47 +0900

    add: AuthConsumer websocket_receive

--- main/consumers.py
+++ main/consumers.py
@@ -3,6 +3,9 @@ import json
 from django.contrib.auth import get_user_model
 from channels.consumer import SyncConsumer, AsyncConsumer
 
+AUTHRES = 'res'
+AUTHREQ = 'req'
+
 class WriteConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         self.board_room = '123'
@@ -32,7 +35,27 @@ class WriteConsumer(AsyncConsumer):
 
 class AuthConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
-        
+        await self.send({
+            "type": "websocket.accept"
+        })
 
     async def websocket_receive(self, event):
+        action = event.get('action', None)
+        board = event.get('board', None)
+        user = event.get('user', None)
+
+        if action == AUTHREQ:
+
+
+        elif action == AUTHRES:
+            accept = event.get('accept', None)
+
+        await self.channel_layer.group_send(
+            self.board_room,
+            {
+            "type": "board_message",
+            "text": text
+            }
+        )
+
 
