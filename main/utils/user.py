7786f6e03b1fc4d661ba6b8339ce8e29c8d89efd
hanchchch <cndghks15@gmail.com>
2021-01-09 14:29:36 +0900

    fix: create_user

--- main/utils/user.py
+++ main/utils/user.py
@@ -2,7 +2,7 @@ import uuid1
 import random
 
 from main.models import User
-from main.utils.common import Status, Message
+from main.utils.common import Status, Message, is_success
 
 with open("main/fruits.txt", "r") as f:
     fruit_list = f.read().split('\n')
@@ -11,7 +11,7 @@ def create_user(request):
     if request.session.get('id') is None: # First Access
         try:
             user = User(session_id=str(uuid1.uuid1()),
-                        nickname=random.sample(animal_list, 1)[0] # 추후 수정 필요
+                        nickname=random.sample(fruit_list, 1)[0] # 추후 수정 필요
                         )
             user.save()
             
