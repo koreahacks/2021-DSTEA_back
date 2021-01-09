e0db3052b26f37299d0af5fb2fb5f661328f40fd
L0Z1K <qortmddbs731@naver.com>
2021-01-09 10:56:32 +0900

    write DB

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from colorfield.fields import ColorField
 
-# Create your models here.
+class Board(models.Model):
+    board_url = models.CharField(max_length=40)
+    admin_id = models.CharField(max_length=40, null=True)
+
+class Path(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    page_id = models.IntegerField()
+    session_id = models.CharField(max_length=40)
+    is_public = models.BooleanField(default=False)
+    pen_type = models.IntegerField(
+        choices = [(0, 'pencil'),(1, 'pen'),(2, 'highlight')],
+        default = 0
+    )
+    color = ColorField(default="#000000")
+    data = models.TextField()
+
+class User(models.Model):
+    session_id = models.CharField(max_length=40)
+    nickname = models.CharField(max_length=10)
+    board = models.ForeignKey(Board, null=True, on_delete=models.CASCADE)
+    auth_write = models.BooleanField(default=True)
